718e1970772c1070c7a13b5f88ed08feaa9db09f
kingsley olawoye <you@example.com>
2023-03-04 22:20:17 +0100

    remove some files

---
+++ script.js
@@ -0,0 +1,8 @@
+// let myName = 'kingsley'
+//  myName = 'olawoye'
+// let age = '22'
+// const phone = "08140274679"
+
+// let statement = `my name is ${myName} and i am ${age} my phone number is ${phone}`
+// alert(statement)
+
